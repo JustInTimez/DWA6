4d3d3f788943e452a87c2c27c4f7311a07c0be46
JustInTimez <just.oost@gmail.com>
2023-04-28 16:56:16 +0200

    DWA8 Encapsulates and makes render an object

--- scripts.js
+++ scripts.js
@@ -4,44 +4,49 @@ import { addEventListeners } from './listeners.js';
 let page = 1;
 let matches = books;
 
-const starting = document.createDocumentFragment();
+
 
 /**
- * Renders a set of book previews onto the page.
- *
- * @param {Object[]} matches - The list of books to render previews for.
- * @param {string} matches[].author - The author of the book.
- * @param {string} matches[].id - The unique ID of the book.
- * @param {string} matches[].image - The URL of the book's cover image.
- * @param {string} matches[].title - The title of the book.
- * @param {number} numPerPage - The number of book previews to render per page.
+ * Book previews object that renders the display of the books
  */
-function renderBookPreviews(matches, numPerPage) {
-  // Only render the first numPerPage matches
-  for (const { author, id, image, title } of matches.slice(0, numPerPage)) {
-    const element = document.createElement("button");
-    element.classList = "preview";
-    element.setAttribute("data-preview", id);
+ const BookPreviewRenderer = {
+  /**
+   * Renders a set of book previews onto the page.
+   * @param {Object[]} matches - The list of books to render previews for.
+   * @param {string} matches[].author - The author of the book.
+   * @param {string} matches[].id - The unique ID of the book.
+   * @param {string} matches[].image - The URL of the book's cover image.
+   * @param {string} matches[].title - The title of the book.
+   * @param {number} numPerPage - The number of book previews to render per page.
+   */
+  renderBookPreviews(matches, numPerPage) {
+    const starting = document.createDocumentFragment();
+    // Only render the first numPerPage matches
+    for (const { author, id, image, title } of matches.slice(0, numPerPage)) {
+      const element = document.createElement("button");
+      element.classList = "preview";
+      element.setAttribute("data-preview", id);
 
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `;
+      element.innerHTML = `
+          <img
+              class="preview__image"
+              src="${image}"
+          />
+          
+          <div class="preview__info">
+              <h3 class="preview__title">${title}</h3>
+              <div class="preview__author">${authors[author]}</div>
+          </div>
+      `;
 
-    starting.appendChild(element);
+      starting.appendChild(element);
+    }
+    document.querySelector("[data-list-items]").appendChild(starting);
   }
 }
 
 // Call the function to render the book previews
-renderBookPreviews(matches, BOOKS_PER_PAGE);
-document.querySelector("[data-list-items]").appendChild(starting);
+BookPreviewRenderer.renderBookPreviews(matches, BOOKS_PER_PAGE);
 
 
 
