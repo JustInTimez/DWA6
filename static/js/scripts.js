afdf853b1fbf78754e7ccab98474e32c96fba29a
JustInTimez <just.oost@gmail.com>
2023-05-03 18:55:37 +0200

    Adds showmore button component

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,15 @@
-import { books, authors, genres, BOOKS_PER_PAGE } from '../js/information/data.js';
-import { addEventListeners } from '../js/listeners/listeners.js';
-import { BookPreviewRenderer } from '../js/components/renderer.js';
-import { AuthorsForm } from '../js/components/authors-form.js';
+import {
+  books,
+  authors,
+  BOOKS_PER_PAGE,
+} from "../js/information/data.js";
+import { addEventListeners } from "../js/listeners/listeners.js";
+import { BookPreviewRenderer } from "../js/components/renderer.js";
+import { AuthorsForm } from "../js/components/authors-form.js";
+import { ShowMoreButton } from "../js/components/show-more.js";
 
 let page = 1;
 export let matches = books;
-
 
 // Create a BookPreviewRenderer instance
 const bookPreviewRenderer = new BookPreviewRenderer(authors);
@@ -13,17 +17,14 @@ const bookPreviewRenderer = new BookPreviewRenderer(authors);
 // Call the function to render the book previews
 bookPreviewRenderer.renderBookPreviews(matches, BOOKS_PER_PAGE);
 
-
-
 // Call the method to create genre options
 const authorsForm = new AuthorsForm(authors);
 authorsForm.generate();
 
-
-
-
-// Call the functions to execute the code
+// Call the function to update the "Show more" button
+const showMoreButton = new ShowMoreButton();
 showMoreButton.update(page, BOOKS_PER_PAGE, books.length, matches);
+
 addEventListeners();
 
 export { page };
